ef68a6f1429c59f637ca78dd6e3c541d18b31f15
katam.bala <katam.bala@phonepe.com>
2024-09-28 09:52:29 +0530

    feature/Basic File reader

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 use std::{
-    error::Error,
-    io::{stdin, stdout, Stdout, Write},
+    cmp::min,
+    env,
+    fs::File,
+    io::{self, stdin, stdout, BufRead, Stdout, Write},
+    path::PathBuf,
 };
 
 use termion::{
@@ -8,13 +11,22 @@ use termion::{
     event::Key,
     input::TermRead,
     raw::{IntoRawMode, RawTerminal},
-    screen::{self, AlternateScreen, IntoAlternateScreen},
+    screen::{AlternateScreen, IntoAlternateScreen, ToMainScreen},
     terminal_size,
 };
+#[derive(Debug)]
 
 struct Position {
     x: u16,
     y: u16,
+}
+
+#[derive(Debug)]
+struct Content {
+    file_content: Vec<String>,
+    num_lines: u16,
+    row_off: u16,
+    col_off: u16,
 }
 
 struct Editor {
@@ -22,10 +34,12 @@ struct Editor {
     screen: AlternateScreen<RawTerminal<Stdout>>,
     buffer: Vec<u8>,
     cursor_position: Position,
+    content: Content,
+    refresh: bool,
 }
 
 impl Editor {
-    fn initalize() -> Editor {
+    fn initalize(content: Content) -> Editor {
         return Editor {
             should_quit: false,
             screen: stdout()
@@ -34,7 +48,9 @@ impl Editor {
                 .into_alternate_screen()
                 .unwrap(),
             buffer: Vec::new(),
-            cursor_position: Position { x: 1, y: 1 },
+            cursor_position: Position { x: 2, y: 3 },
+            content,
+            refresh: false,
         };
     }
     fn move_cursor(&mut self, n_x: u16, n_y: u16) -> Result<(), std::io::Error> {
@@ -43,8 +59,72 @@ impl Editor {
         self.cursor_position.y = n_y;
         Ok(())
     }
-    fn handle_key_event(&mut self, event: Key) -> Result<(), std::io::Error> {
+    fn set_cursor(&mut self, n_x: u16, n_y: u16) {
+        // print!("{}{}", n_x, n_y);
+        self.cursor_position.x = n_x;
+        self.cursor_position.y = n_y;
+    }
+    fn handle_scroll(&mut self, current_line_index: usize) -> Result<(), std::io::Error> {
         let (col, row) = terminal_size()?;
+        let y_limit = min(self.content.num_lines - self.content.row_off + 3, row);
+        let current_line_len = self.content.file_content[current_line_index].len() as u16;
+        let x_limit = min(current_line_len - self.content.col_off + 2, col);
+        if self.cursor_position.y >= y_limit {
+            self.content.row_off = min(self.content.num_lines - 1, self.content.row_off + 1);
+            self.set_cursor(self.cursor_position.x, self.cursor_position.y - 1);
+            self.refresh = true;
+        }
+        if self.cursor_position.y < 3 {
+            self.content.row_off = if self.content.row_off > 0 {
+                self.content.row_off - 1
+            } else {
+                0
+            };
+            self.set_cursor(self.cursor_position.x, self.cursor_position.y + 1);
+            self.refresh = true;
+        }
+        if self.cursor_position.x >= x_limit {
+            if x_limit == 2 {
+                self.set_cursor(self.cursor_position.x - 1, self.cursor_position.y);
+            } else {
+                self.content.col_off = min(current_line_len - 1, self.content.col_off + 1);
+                self.set_cursor(self.cursor_position.x - 1, self.cursor_position.y);
+                self.refresh = true;
+            }
+        }
+        if self.cursor_position.x < 2 {
+            self.content.col_off = if self.content.col_off > 0 {
+                self.content.col_off - 1
+            } else {
+                0
+            };
+            self.set_cursor(self.cursor_position.x + 1, self.cursor_position.y);
+            self.refresh = true;
+        }
+        self.move_cursor(self.cursor_position.x, self.cursor_position.y)?;
+        Ok(())
+    }
+    fn handle_arrrow_keys(&mut self, event: Key) -> Result<(), std::io::Error> {
+        let current_line_index = (self.cursor_position.y - 3 + self.content.row_off) as usize;
+        match event {
+            Key::Left => {
+                self.set_cursor(self.cursor_position.x - 1, self.cursor_position.y);
+            }
+            Key::Right => {
+                self.set_cursor(self.cursor_position.x + 1, self.cursor_position.y);
+            }
+            Key::Up => {
+                self.set_cursor(self.cursor_position.x, self.cursor_position.y - 1);
+            }
+            Key::Down => {
+                self.set_cursor(self.cursor_position.x, self.cursor_position.y + 1);
+            }
+            _ => {}
+        }
+        self.handle_scroll(current_line_index)?;
+        Ok(())
+    }
+    fn handle_key_event(&mut self, event: Key) -> Result<(), std::io::Error> {
         match event {
             Key::Ctrl(c) => {
                 if c == 'a' {
@@ -56,30 +136,14 @@ impl Editor {
             Key::Char(v) => {
                 if v != '\n' && v != '\r' && v != '\t' {
                     write!(self.screen, "{v}")?;
+                    self.set_cursor(self.cursor_position.x + 1, self.cursor_position.y);
                 }
                 if v == '\n' {
-                    write!(self.screen, "\r\n ")?;
+                    self.move_cursor(2, self.cursor_position.y + 1)?;
                 }
             }
-            Key::Left => {
-                if self.cursor_position.x > 2 {
-                    self.move_cursor(self.cursor_position.x - 1, self.cursor_position.y)?;
-                }
-            }
-            Key::Right => {
-                if self.cursor_position.x < col {
-                    self.move_cursor(self.cursor_position.x + 1, self.cursor_position.y)?;
-                }
-            }
-            Key::Up => {
-                if self.cursor_position.y > 3 {
-                    self.move_cursor(self.cursor_position.x, self.cursor_position.y - 1)?;
-                }
-            }
-            Key::Down => {
-                if self.cursor_position.y < row - 5 {
-                    self.move_cursor(self.cursor_position.x, self.cursor_position.y + 1)?;
-                }
+            Key::Up | Key::Down | Key::Left | Key::Right => {
+                self.handle_arrrow_keys(event)?;
             }
             Key::Delete => {}
             _ => {}
@@ -104,45 +168,55 @@ impl Editor {
             cursor::Goto(1, 3),
             clear::CurrentLine
         )?;
-        let (mut _col, row) = termion::terminal_size()?;
-        for _i in 0..row - 10 {
-            write!(self.buffer, "~\r\n")?;
+        let (col, row) = termion::terminal_size()?;
+        for i in 3..row {
+            let line_index = (i - 3) + self.content.row_off;
+            let threshold = (col - 2 + self.content.col_off) as usize;
+            write!(self.buffer, "{}", clear::CurrentLine)?;
+            if let Some(line) = self.content.file_content.get(line_index as usize) {
+                let current_line_len = line.len() as u16;
+                if current_line_len <= self.content.col_off {
+                    write!(self.buffer, " \r\n")?;
+                } else {
+                    let slice = &line[(self.content.col_off as usize)..line.len().min(threshold)];
+                    write!(self.buffer, " {}\r\n", slice)?;
+                }
+            }
         }
-        write!(self.buffer, "{}", cursor::Goto(2, 3))?;
-        self.cursor_position.x = 2;
-        self.cursor_position.y = 3;
         Ok(())
     }
     fn refresh_screen(&mut self) -> Result<(), std::io::Error> {
         // hide the cursor
-        write!(self.buffer, "{}", cursor::Hide)?;
+        write!(self.buffer, "{}{}", cursor::Hide, cursor::Goto(1, 1))?;
         // move the cursor to the top left corner
-        write!(self.buffer, "{}", cursor::Goto(1, 1))?;
+        // write!(self.buffer, "{}", cursor::Goto(1, 1))?;
         // welcome message
         self.display_hello_message()?;
         self.draw_rows()?;
         // show cursor
-        write!(self.buffer, "{}", cursor::Show)?;
+        write!(
+            self.buffer,
+            "{}{}",
+            cursor::Goto(self.cursor_position.x, self.cursor_position.y),
+            cursor::Show
+        )?;
         self.screen.write_all(&self.buffer)?;
+        self.screen.flush()?;
         self.buffer.clear();
         Ok(())
     }
     fn render(&mut self) -> Result<(), std::io::Error> {
-        // write!(
-        //     self.screen,
-        //     "{}{}",
-        //     termion::clear::All,
-        //     termion::cursor::Goto(1, 1)
-        // )?;
-
-        // self.display_hello_message()?;
-        // self.draw_rows()?;
         self.refresh_screen()?;
         self.screen.flush()?;
 
         for input in stdin().keys() {
             if let Ok(event) = input {
                 self.handle_key_event(event)?;
+            }
+            if self.refresh {
+                print!("{}", self.content.col_off);
+                self.refresh_screen()?;
+                self.refresh = false;
             }
             if self.should_quit {
                 break;
@@ -153,6 +227,48 @@ impl Editor {
 }
 
 fn main() {
-    let mut editor = Editor::initalize();
-    editor.render().unwrap();
+    std::panic::set_hook(Box::new(|info| {
+        println!("Code Paniked!! {info}");
+    }));
+
+    let args: Vec<String> = env::args().collect();
+
+    // Ensure a file name argument is provided
+    if args.len() < 2 {
+        eprintln!("Usage: {} <filename>", args[0]);
+        return;
+    }
+
+    let current_dir = env::current_dir().unwrap();
+
+    // Get the file name from the arguments
+    let file_name = &args[1];
+
+    // Construct the file path relative to the current directory
+    let mut file_path = PathBuf::from(current_dir);
+    file_path.push(file_name);
+
+    // Open the file
+    let file = File::open(file_path).unwrap();
+
+    // Create a BufReader to read the file line by line
+    let reader = io::BufReader::new(file);
+    let lines: Vec<String> = reader.lines().collect::<Result<_, _>>().unwrap();
+    let num_lines = lines.len() as u16;
+    let content: Content = Content {
+        file_content: lines,
+        row_off: 0,
+        col_off: 0,
+        num_lines,
+    };
+    // println!("{:?}", content);
+    let mut editor = Editor::initalize(content);
+    match editor.render() {
+        Ok(_) => {}
+        Err(err) => {
+            write!(stdout(), "{}", ToMainScreen).expect_err("Failed to move to main screen");
+            println!("{:?}", err);
+            loop {}
+        }
+    }
 }
